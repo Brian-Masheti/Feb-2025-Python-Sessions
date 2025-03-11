f160e7da82fa29c4f27a3f89df93e5c861918afc
Brian-Masheti <brianxmasheti>
2025-03-11 12:01:07 +0300

    Introduction to Python

--- Week 1/index.py
+++ Week 1/index.py
@@ -9,4 +9,6 @@ marks = 10.5 # Int DataType
 
 name = 'John Doe' # String dataType
 
-language = ['Python', 'Java', 'C++'] # List DataType
+language = ['Python', 'Java', 'C++', 3, 5.7, True] # List DataType
+print(language)
+print(language[-3])
