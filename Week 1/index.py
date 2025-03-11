7e963d8a8bf60a1d52f62ed2e75f9e3232f6f12c
Brian-Masheti <brianxmasheti>
2025-03-11 11:30:45 +0300

    Intro to python

--- Week 1/index.py
+++ Week 1/index.py
@@ -3,3 +3,10 @@ print('Hello World')
 number = 10
 print(number)
 
+# Data Types
+
+marks = 10.5 # Int DataType
+
+name = 'John Doe' # String dataType
+
+language = ['Python', 'Java', 'C++'] # List DataType
