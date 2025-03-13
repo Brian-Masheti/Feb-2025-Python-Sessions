1bf688609490ae279c1a3b83479907ae7a7564f7
Brian-Masheti <brianxmasheti>
2025-03-13 20:12:57 +0300

    A simple greetings app

--- Week 1/simplegreetingapp.py
+++ Week 1/simplegreetingapp.py
@@ -1,7 +1,7 @@
 # A simple greeting app that asks for the user's name and favorite color and prints a personalized message.
 
-name = input('What is your name?')
-color = input('What is your favorite color?')
+name = input('What is your name?  🖊️')
+color = input('What is your favorite color?  🎨')
 
 # Print a personalized message
 print(f'Hello, {name}! Your favorite color, {color}, is awesome! 🎨')
@@ -9,4 +9,4 @@ print(f'Hello, {name}! Your favorite color, {color}, is awesome! 🎨')
 # Output
 # What is your name?JBrian
 # What is your favorite color?Blue  
-# Hello, John Doe! Your favorite color, Blue, is awesome! 🎨
+# Hello, Brian! Your favorite color, Blue, is awesome! 🎨
